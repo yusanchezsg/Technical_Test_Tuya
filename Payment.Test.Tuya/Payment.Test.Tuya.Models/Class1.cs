bac19e56844e82cb0d79c8c42acfed88c240190f
jecheverry <juan85.echeverry@gmail.com>
2022-04-30 16:17:44 -0500

    The model is created with the following properties

--- Payment.Test.Tuya/Payment.Test.Tuya.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Payment.Test.Tuya.Models
-{
-    public class Class1
-    {
-    }
-}
---
+++ Payment.Test.Tuya/Payment.Test.Tuya.Models/Models/Products.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Payment.Test.Tuya.Models
+{
+    [Table("PRODUCTS")]
+    public class Products
+    {
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity), Key()]
+        [Column("ID", Order = 0)]
+        public int ProductId { get; set; }
+
+        [Column("NAME", Order = 1)]
+        public string ProductName{ get; set; }
+
+        [Column("VALUE", Order = 2)]
+        public float ProductValue { get; set; }
+    }
+}
